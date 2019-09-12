cc398f1601179923d7171c1edc8ff0c846d87b69
sine <963366202@qq.com>
2019-09-13 02:34:59 +0800

    add data types

---
+++ client/src/types.ts
@@ -0,0 +1,95 @@
+export interface University {
+  name: string
+  abbreviation: string[]
+  location?: {
+    latitude: number
+    longitude: number
+  }
+}
+
+interface TOEFLScore {
+  reading: number
+  listening: number
+  speaking: number
+  writing: number
+  total: number
+}
+
+interface GREScore {
+  verbal: number
+  quant: number
+  AW: number
+  total: number
+}
+
+interface GPA {
+  scale_4?: number
+  scale_4_3?: number
+  scale_100?: number
+}
+
+export interface Program {
+  university: string
+  name: string
+  abbreviations: string[]
+  timeline: {
+    opening: Date | null
+    early_deadline: Date | null
+    fianl_deadline: Date | null
+  }
+  TOEFL: {
+    minimum: TOEFLScore
+    institution_code: string
+    department_code: string
+  }
+  GRE: {
+    minimum: GREScore
+    institution_code: string
+    department_code: string
+  }
+  GPA: {
+    minumum: GPA
+  }
+  cost: {
+    semester: number
+    tuition_per_semester: number
+  }
+  website: string
+}
+
+export interface Applicant {
+  id: string
+  diploma: 'bachelor' | 'graduate'
+  GRE: GREScore
+  TOEFL: TOEFLScore
+  bachelor: {
+    major: string
+    gpa: GPA
+    university_level: string
+    university: string
+  }
+  graduate?: {
+    major: string
+    gpa: GPA
+    university_level: string
+    university: string
+  }
+  background: string
+}
+
+export interface Application {
+  applicant: string
+  year: string
+  university: string
+  degree: string
+  program: string
+  semester: string
+  result: string
+  country: string
+  info: string
+  url: string
+  timeline: {
+    inform_time: string
+    submit_time: string
+  }
+}
